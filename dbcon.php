09d269086808630ee6162b192b7254be70be5921
Stebbi <stebbi1414@gmail.com>
2016-05-10 12:20:56 +0000

    asd

--- dbcon.php
+++ dbcon.php
@@ -9,7 +9,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
 
 $sql = "SELECT lid1_lid2, date, bo FROM leikir";
 $result = $conn->query($sql);
