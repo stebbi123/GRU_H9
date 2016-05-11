daf7596c97e133dc83165427d2cd7a9702d096ac
Stebbi <stebbi1414@gmail.com>
2016-05-11 15:26:01 +0000

    smá bug fix með lið sem hafa v í nafninu

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $('document').ready(function(){
   $('.list-group-item').click(function(){
   	var index = $(this).text();
-  	var array = index.replace("|", "v").split("v");
+  	var array = index.replace("|", " v ").split(" v ");
   	$('.slidedown').text(array[0] + " vs " + array[1]);
     $('#fela').slideToggle();
   });
