ad450f0a82cf156c48cab52692c18bb310b6e073
Stefán Atli Ágústsson <stefanag261@nemi.tskoli.is>
2016-05-12 13:48:56 +0000

    modal jquery fun

--- js/scripts.js
+++ js/scripts.js
@@ -9,14 +9,30 @@ $('document').ready(function(){
     $('.btnlid2').text(array[1]);
     $('.chcklid2').prop('checked', false)
     $('.chcklid1').prop('checked', false)
+    $('.check1').hide();
+    $('.check2').hide();
   });
   $('.btnlid1').click(function(){
-  	$('.chcklid1').trigger('click');
-  	$('.chcklid2').prop('checked', false)
+    var index = $(this).text();
+    $('.check2').hide();
+    $('.jsdate').hide();
+    $('.jsleikir').hide();
+    $('.jspick').text("Val Móttekið");
+    $('.jspick2').text("Þú hefur valið " + index);
+    $(".check1").show();
+  	//$('.chcklid1').trigger('click');
+  	//$('.chcklid2').prop('checked', false)
   });
     $('.btnlid2').click(function(){
-  	$('.chcklid2').trigger('click');
-  	$('.chcklid1').prop('checked', false)
+    var index = $(this).text();
+    $('.check1').hide();
+    $('.jsdate').hide();
+    $('.jsleikir').hide();
+    $('.jspick').text("Val Móttekið");
+    $('.jspick2').text("Þú hefur valið " + index);
+    $(".check2").show();
+  	//$('.chcklid2').trigger('click');
+  	//$('.chcklid1').prop('checked', false)
   });
 });
 
